5d9ffb830584104ca98442dee7823ab6628fcf5a
apenkbung <apenkbung@gmail.com>
2019-08-06 08:39:33 +0700

    Generate Question model via artisan model generator2

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function question()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
