f0663af763d6d9d4c79e437bbce21b82fe2fc872
ondozwahyudi <ondozwahyudi@gmail.com>
2019-08-08 00:55:50 +0700

    test bosku

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -2,32 +2,6 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">All Questions</div>
-                <div class="card-body">
-                   @foreach ($questions as $question)
-                    <div class="media">
-                        <div class="media-body">
-                            <h3 class="mt-0">
-                                <a href="{{ $question->url }}">{{ $question->title }}</a>
-                            </h3>
-                            <p class="lead">
-                                Asked by
-                                <a href=" {{ $question->user->url }} ">{{ $question->user->name }}</a>
-                                <br>
-                                <small class="text-muted">{{ $question->created_date}}</small>
-                            </p>
-                            {{ str_limit($question->body, 250) }}
-                        </div>
-                    </div>
-                    <hr>
-                   @endforeach
-                   {{ $questions->links() }}
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
 @endsection
