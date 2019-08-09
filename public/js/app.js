b5e6696fc5f0dae86f3e3a3f33fcf46f462389e0
ondozwahyudi <ondozwahyudi@gmail.com>
2019-08-09 09:52:59 +0700

    Deleteing The Question

--- public/js/app.js
+++ public/js/app.js
@@ -49366,8 +49366,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\laravel-qa\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\laravel-qa\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/laravel-qa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/laravel-qa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
